1a1f5003a7eea091e004f71f00c4d546da1eac41
sanketsushounen790 <soisanbongdem999@gmail.com>
2022-07-27 17:47:48 -0400

    change the annouce when bot server  is up

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ import dotenv from "dotenv"
 dotenv.config()
 const port = process.env.PORT || 3500
 const uri = process.env.LUCY_LAWSON_URI
-
+const lucyHelloWords = [
+    "Biết rồi đang dậy nè ! Từ từ !",
+    "CC kêu hoài. Dậy rồi đây !",
+    "Đang ngủ ngon dm ! Giờ thức luôn !",
+    "Đang ngủ ngon haizz ! Hên cho mày là nay tao không quạu đó !",
+    "Ayo Lucy thức dậy rồi đây !",
+    "Mịa đang ngủ ngon ! Có việc gì mà đánh thức tao đó !"
+]
 //Middlewares
 const app = express()
 app.use(express.json())
@@ -25,10 +32,9 @@ mongoose.connect(uri , {
     console.log(err)
 })
 
-
 //API Endpoints
 app.get("/", (req, res)=>{
-    res.status(200).send("Bot server is running")
+    res.status(200).send(`${lucyHelloWords[Math.floor(Math.random() * lucyHelloWords.length)]}\nQuay lại discord đi !`)
 })
 
 //Start server 
